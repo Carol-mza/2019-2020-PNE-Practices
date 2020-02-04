e11f9660da42b420491d1033ee9be73919b6ccf5
Carol-mza <c.manso.2019@alumnos.urjc.es>
2020-02-04 16:07:44 +0100

    Session 2

---
+++ Session 01/count.py
@@ -0,0 +1,4 @@
+# Sesion 1. Exercise 2
+
+for i in range(1, 21):
+    print(i, end = " ")
---
+++ Session 01/sum20.py
@@ -0,0 +1,12 @@
+# Sesion 1. Exercise 3
+# Calculate the sum of the 20 first integer numbers
+# 1 + 2 + 3 + ... + 20
+# Example working
+# -- Store the result
+
+res = 0
+
+for i in range("1, 21"):
+    res += 1
+
+print("Total sum: ", res)
